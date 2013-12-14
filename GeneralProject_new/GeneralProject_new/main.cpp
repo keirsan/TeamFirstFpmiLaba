52c11d4f56e65344e54699173c1a0e0a1269aacc
vitux <vitux1@mail.ru>
2013-12-14 15:09:14 +0300

    bug

--- GeneralProject_new/GeneralProject_new/main.cpp
+++ GeneralProject_new/GeneralProject_new/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include "BigInt.h"
+#include "Rational.h"
+
 // For try, that I won't brake anything
 int main()
 {
+    Rational a(2,3);
+    Rational b(1,6);
+    a = a + b;
+    cout << a;
     return 0;
 }

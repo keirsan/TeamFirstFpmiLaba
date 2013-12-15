4dc4fec4da26b60ea985ab279ecdd7cbf9f487b8
apsent1 <klukina_anna@mail.ru>
2013-12-15 15:42:59 +0300

    Calc

--- GeneralProject_new/GeneralProject_new/Calc.h
+++ GeneralProject_new/GeneralProject_new/Calc.h
@@ -1,5 +1,3 @@
-// Объяснение , как работает класс, вконце
-
 #include<iostream>
 #include<stack>
 #include<cstring>
@@ -38,44 +36,9 @@ class Calc
     void writeOpSecond_toStack(int &, char expressionConv[], char expression[]);
     void writeCloseBrecket(int &, char expressionConv[], char expression[]);
 
-    double searchV(char n[], bool&);
     bool reading(char expression[]);
     bool reformation(char expressionConv[]);
-    void destruct();
-
-public:
-    struct Variable
-    {
-        double value;
-        char name[100];
-        Variable * next;
-    };
-    void pushV(char V[], bool&);
 
 public:
     double result(char expressionConv[]);    
 };
-
-
-
-/*     Функции result() передаётся строка, которая превращается в строку ОПЗ в функции reformaton()
-При преобразовании числа и переменные записываются в строку сразу, а операторы помещаются в стек. У операторов *, / приоритет выше, чем у + и -. При переводе проверяет правильность расстановки круглых скобок.
-
-Функция reading() читает ОПЗ и вычисляет. Числа загоняются в стек, а операции выполняются сразу после их нахождения.
-
-Пример: expressionConv[] = 5*2.3-1
-Функция идёт по строке
-i = 0: 5 - записываем в новую строку. expression[] = 5
-i = 1: * - заносим в стек операторов. expression[] = 5
-i = 2: 2 - записываем в новую строку. expression[] = 5 2
-i = 3: . - записываем в новую строку. expression[] = 5 2.
-i = 4: 3 - записываем в новую строку. expression[] = 5 2.3 
-i = 5: - - проверяем последний оператор в стеке, если это оператор, который выше по приоритету, записываем его в строку. Т.к. последнее - это умножение, то в строку записываем *. expression[] = 5 2.3 *
-i = 6: 1 - записываем в новую строку. expression[] = 5 2.3 * 1
-После всего оставшиеся операторы записываем в строку
-Конечная ОПЗ -  expression[] = 5 2.3 * 1 -
- ОПЗ считаем, знося в стек числаи попутно выполняя действия операторов
-*/
-
-
-

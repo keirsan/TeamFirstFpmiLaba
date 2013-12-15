92665f11c223222db1fad0cbcf6b6cd31885a9e7
apsent1 <klukina_anna@mail.ru>
2013-12-15 15:29:46 +0300

    Update Calc.h

--- GeneralProject_new/GeneralProject_new/Calc.h
+++ GeneralProject_new/GeneralProject_new/Calc.h
@@ -6,6 +6,8 @@
 #include<math.h>
 #include<ctype.h>
 
+#include"Dictionary.h"
+
 using namespace std;
 
 class Calc
@@ -19,31 +21,27 @@ class Calc
 
         //   Functions
 
-    void recording_toStack(int &, char expression[], stack <double> & buffer);
-    void buf_sin(int &, char expression[], stack <double> & buffer);
-    void buf_sqrt(int &, char expression[], stack <double> & buffer);
-    void buf_cos(int &, char expression[], stack <double> & buffer);
-    void buf_abs(int &, char expression[], stack <double> & buffer);
-    void buf_log(int &, char expression[], stack <double> & buffer);
-    void buf_pow(int &, char expression[], stack <double> & buffer);
-    void realizeFunctions(int &, char expression[], stack <double> & buffer);
-    void use_Variable(int &, char expression[], stack <double> & buffer);
+    void recording_toStack(int &, char expression[]);
+    void use_Variable(int &, char expression[]);
 
-    void oper_mult(int &, char expression[], stack <double> & buffer);
-    void oper_div(int &, char expression[], stack <double> & buffer);
-    void oper_addit(int &, char expression[], stack <double> & buffer);
-    void oper_subtr(int &, char expression[], stack <double> & buffer);
-    void realizeOperators(int &, char expression[], stack <double> & buffer);
-    void realiseBinOper(int, char expression[], stack <double> & buffer);
+    void oper_mult(int &, char expression[]);
+    void oper_pow(int &, char expression[]);
+    void oper_addit(int &, char expression[]);
+    void oper_subtr(int &, char expression[]);
+    void realizeOperators(int &, char expression[]);
+    void realiseBinOper(int, char expression[]);
     
     //---------------------------------------------
 
-    void writeF_toStack(int &, char expressionConv[], char expression[], stack <char> & operands);
-    void writeF_toExpression(char expressionConv[], char expression[], stack <char> & operands);
-    void inputNumbers(int &, char expressionConv[], char expression[], stack <char> & operands);
-    void writeOpFirst_toStack(int &, char expressionConv[], char expression[], stack <char> & operands);
-    void writeOpSecond_toStack(int &, char expressionConv[], char expression[], stack <char> & operands);
-    void writeCloseBrecket(int &, char expressionConv[], char expression[], stack <char> & operands);
+    void inputNumbers(int &, char expressionConv[], char expression[]);
+    void writeOpFirst_toStack(int &, char expressionConv[], char expression[]);
+    void writeOpSecond_toStack(int &, char expressionConv[], char expression[]);
+    void writeCloseBrecket(int &, char expressionConv[], char expression[]);
+
+    double searchV(char n[], bool&);
+    bool reading(char expression[]);
+    bool reformation(char expressionConv[]);
+    void destruct();
 
 public:
     struct Variable
@@ -54,15 +52,8 @@ public:
     };
     void pushV(char V[], bool&);
 
-private: 
-    double searchV(char n[], bool&);
-    bool reading(char expression[]);
-    bool reformation(char expressionConv[]);
-    void destruct();
-
 public:
-    double result(char expressionConv[]);
-    
+    double result(char expressionConv[]);    
 };
 
 

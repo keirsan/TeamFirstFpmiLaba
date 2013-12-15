8f81ce582a73165f6603653607f400f921431082
vitux <vitux1@mail.ru>
2013-12-15 20:37:56 +0300

    Откат

--- GeneralProject_new/GeneralProject_new/main.cpp
+++ GeneralProject_new/GeneralProject_new/main.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
 #include "BigInt.h"
 #include "Rational.h"
+#include "Calc.h"
 using namespace std;
 
 // For try, that I won't brake anything
 int main()
 {
-    BigInt c(228);
-    cout << (c ^ 1488) << endl; 
-    Rational a(2,3);
-    Rational b(1,6);
-    a = a + b;
-    cout << a;
+    Calc a;
+    cout<<a.result("2+3");
     return 0;
 }

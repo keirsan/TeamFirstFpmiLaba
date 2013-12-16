d226ee7cd22148c1bd338c0c40362e6ad4c4c9f1
apsent1 <klukina_anna@mail.ru>
2013-12-16 12:12:50 +0300

    Update Dictionary.h

--- GeneralProject_new/GeneralProject_new/Dictionary.h
+++ GeneralProject_new/GeneralProject_new/Dictionary.h
@@ -22,8 +22,7 @@ public:
 	Node *getRoot()
 	{ return root;}
 	void add(int value, string c);
-	int search(string value);
+	int search(string value, bool&);
 	void show(Node *root, int level);
 	void delTree(Node *root);
 };
-

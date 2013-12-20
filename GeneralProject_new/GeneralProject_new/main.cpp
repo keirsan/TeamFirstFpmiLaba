477814eff22eb26aefcdd6b662c6cd14c98ad06a
ymurashka <7735897@gmail.com>
2013-12-20 18:54:25 +0300

    Refactored command line arguments

--- GeneralProject_new/GeneralProject_new/main.cpp
+++ GeneralProject_new/GeneralProject_new/main.cpp
@@ -4,33 +4,35 @@
 #include "Calc.h"
 #include "Polynom.h"
 #include "Test.h"
-#include <Windows.h>
-#include <stdlib.h>
+
 using namespace std;
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
-    if (argv[1])
+    // process command line arguments
+    if (argc>1)
     {
-        if (argv[1][0] == '-'&&argv[1][1] == 't'&&argv[1][2] == 'e'&&argv[1][3] == 's'&&argv[1][4] == 't'&&argv[1][5] == 's')
+        if (argc == 2 && !strcmp(argv[1], "-tests"))
         {
             Test testing;
             testing.testingProcess();            
             system("pause");
+            return 0;
         }
+        cout<<"Use -tests to run tests."<<endl;
+        return 0;
     }
-    else
+
+    // run calculator
+    while (true)
     {
-        while (true)
-        {
-            Calc a;
-            char * str = new char[];
-            cin >> str;
-            if (str[0] == 'e')
-                break;
-            cout << a.result(str) << endl;
-        }
+        Calc a;
+        char * str = new char[];
+        cin >> str;
+        if (str[0] == 'e')
+            break;
+        cout << a.result(str) << endl;
     }
-    
+
     return 0;
 }

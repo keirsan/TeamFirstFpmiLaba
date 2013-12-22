47d05cabb0f3125e149436e6ecb97d4d90646195
Graf44 <artsen444@gmail.com>
2013-12-22 19:31:49 +0300

    Complex - and *, also <<

--- GeneralProject_new/GeneralProject_new/Complex.h
+++ GeneralProject_new/GeneralProject_new/Complex.h
@@ -11,17 +11,16 @@ class Complex
 	type1 re;
 	type1 im;
     Complex add(Complex &c);
-	Complex operator-(Complex &c);
-	Complex operator*(Complex &c);
+	Complex add1(Complex &c);
+	Complex add2(Complex &c);
 public:
 	Complex();
 	Complex(type1 r, type1 i);
 	Complex(type1 r);
 	Complex(Complex &c);
 	~Complex();
-	void show();//temporary
 
-    //friend ostream & operator<<(std::ostream & os, Complex<type1> & number); doesn't work
+    template<class type1> friend ostream & operator<<(std::ostream & os, Complex<type1> & number);
 
     friend Complex<double> operator+(Complex<int> c1, Complex<double> c2);
     friend Complex<double> operator+(Complex<double> c1, Complex<int> c2);
@@ -39,14 +38,156 @@ public:
     friend Complex<BigInt> operator+(Complex<BigInt> c1, Complex<int> c2);
     friend Complex<BigInt> operator+(Complex<BigInt> c1, Complex<BigInt> c2);
     friend Complex<int> operator+(Complex<int> c1, Complex<int> c2);
+
+	friend Complex<double> operator-(Complex<int> c1, Complex<double> c2);
+    friend Complex<double> operator-(Complex<double> c1, Complex<int> c2);
+    friend Complex<double> operator-(Complex<BigInt> c1, Complex<double> c2);
+    friend Complex<double> operator-(Complex<double> c1, Complex<BigInt> c2);
+    friend Complex<double> operator-(Complex<Rational> c1, Complex<double> c2);
+    friend Complex<double> operator-(Complex<double> c1, Complex<Rational> c2);
+    friend Complex<double> operator-(Complex<double> c1, Complex<double> c2);
+    friend Complex<Rational> operator-(Complex<BigInt> c1, Complex<Rational> c2);
+    friend Complex<Rational> operator-(Complex<Rational> c1, Complex<BigInt> c2);
+    friend Complex<Rational> operator-(Complex<int> c1, Complex<Rational> c2);
+    friend Complex<Rational> operator-(Complex<Rational> c1, Complex<int> c2);
+    friend Complex<Rational> operator-(Complex<Rational> c1, Complex<Rational> c2);
+    friend Complex<BigInt> operator-(Complex<int> c1, Complex<BigInt> c2);
+    friend Complex<BigInt> operator-(Complex<BigInt> c1, Complex<int> c2);
+    friend Complex<BigInt> operator-(Complex<BigInt> c1, Complex<BigInt> c2);
+    friend Complex<int> operator-(Complex<int> c1, Complex<int> c2);
+
+	friend Complex<double> operator*(Complex<int> c1, Complex<double> c2);
+    friend Complex<double> operator*(Complex<double> c1, Complex<int> c2);
+    friend Complex<double> operator*(Complex<BigInt> c1, Complex<double> c2);
+    friend Complex<double> operator*(Complex<double> c1, Complex<BigInt> c2);
+    friend Complex<double> operator*(Complex<Rational> c1, Complex<double> c2);
+    friend Complex<double> operator*(Complex<double> c1, Complex<Rational> c2);
+    friend Complex<double> operator*(Complex<double> c1, Complex<double> c2);
+    friend Complex<Rational> operator*(Complex<BigInt> c1, Complex<Rational> c2);
+    friend Complex<Rational> operator*(Complex<Rational> c1, Complex<BigInt> c2);
+    friend Complex<Rational> operator*(Complex<int> c1, Complex<Rational> c2);
+    friend Complex<Rational> operator*(Complex<Rational> c1, Complex<int> c2);
+    friend Complex<Rational> operator*(Complex<Rational> c1, Complex<Rational> c2);
+    friend Complex<BigInt> operator*(Complex<int> c1, Complex<BigInt> c2);
+    friend Complex<BigInt> operator*(Complex<BigInt> c1, Complex<int> c2);
+    friend Complex<BigInt> operator*(Complex<BigInt> c1, Complex<BigInt> c2);
+    friend Complex<int> operator*(Complex<int> c1, Complex<int> c2);
 };
 
-/*template <class type1>
+template <class type1>
 ostream & operator<<(ostream&  out, Complex<type1> & number)
 {
     out << number.re << "+" << number.im <<"i";  
     return out;
-}*/
+}
+//---------------------------------------------------------------------------------------
+//operator -
+
+Complex<double> operator-(Complex<int> c1, Complex<double> c2)
+{
+    Complex<double> temp((double)c1.re,(double)c1.im);
+    return temp.add1(c2);
+}
+
+Complex<double> operator-(Complex<double> c1, Complex<int> c2)
+{
+    Complex<double> temp((double)c2.re,(double)c2.im);
+    return temp.add1(c1);
+}
+
+Complex<double> operator-(Complex<Rational> c1, Complex<double> c2)
+{
+    Complex<double> temp(c1.re.getDouble(), c1.im.getDouble());
+    return temp.add1(c2);
+}
+
+Complex<double> operator-(Complex<double> c1, Complex<Rational> c2)
+{
+    Complex<double> temp(c2.re.getDouble(), c2.im.getDouble());
+    return temp.add1(c1);
+}
+
+Complex<double> operator-(Complex<BigInt> c1, Complex<double> c2)
+{
+    Complex<double> temp((double)c1.re.getLongLong(),(double)c1.im.getLongLong());
+    return temp.add1(c2);
+}
+
+Complex<double> operator-(Complex<double> c1, Complex<BigInt> c2)
+{
+    Complex<double> temp((double)c2.re.getLongLong(),(double)c2.im.getLongLong());
+    return temp.add1(c1);
+}
+
+Complex<double> operator-(Complex<double> c1, Complex<double> c2)
+{
+    return c1.add1(c2);
+}
+
+Complex<Rational> operator-(Complex<BigInt> c1, Complex<Rational> c2)
+{
+    Rational temp1(c1.re.getInt(),1);
+    Rational temp2(c1.im.getInt(),1);
+    Complex<Rational> temp(temp1,temp2);
+    return temp.add1(c2);
+}
+
+Complex<Rational> operator-(Complex<Rational> c1, Complex<BigInt> c2)
+{
+    Rational temp1(c2.re.getInt(),1);
+    Rational temp2(c2.im.getInt(),1);
+    Complex<Rational> temp(temp1,temp2);
+    return temp.add1(c1);
+}
+
+Complex<Rational> operator-(Complex<int> c1, Complex<Rational> c2)
+{
+    Rational temp1(c1.re);
+    Rational temp2(c1.im);
+    Complex<Rational> temp(temp1,temp2);
+    return temp.add1(c2);
+}
+
+Complex<Rational> operator-(Complex<Rational> c1, Complex<int> c2)
+{
+    Rational temp1(c2.re);
+    Rational temp2(c2.im);
+    Complex<Rational> temp(temp1,temp2);
+    return temp.add1(c1);
+}
+
+Complex<Rational> operator-(Complex<Rational> c1, Complex<Rational> c2)
+{
+    return c1.add1(c2);
+}
+
+Complex<BigInt> operator-(Complex<int> c1, Complex<BigInt> c2)
+{
+    BigInt temp1(c1.re);
+    BigInt temp2(c1.im);
+    Complex<BigInt> temp(temp1, temp2);
+    return temp.add1(c2);
+}
+
+Complex<BigInt> operator-(Complex<BigInt> c1, Complex<int> c2)
+{
+    BigInt temp1(c2.re);
+    BigInt temp2(c2.im);
+    Complex<BigInt> temp(temp1, temp2);
+    return temp.add1(c1);
+}
+
+Complex<BigInt> operator-(Complex<BigInt> c1, Complex<BigInt> c2)
+{
+    return c1.add1(c2);
+}
+
+Complex<int> operator-(Complex<int> c1, Complex<int> c2)
+{
+    return c1.add1(c2);
+}
+//---------------------------------------------------------------------------------------
+//operator +
 
 Complex<double> operator+(Complex<int> c1, Complex<double> c2)
 {
@@ -153,6 +294,112 @@ Complex<int> operator+(Complex<int> c1, Complex<int> c2)
 }
 
 //---------------------------------------------------------------------------------------
+//operator *
+
+Complex<double> operator*(Complex<int> c1, Complex<double> c2)
+{
+    Complex<double> temp((double)c1.re,(double)c1.im);
+    return temp.add2(c2);
+}
+
+Complex<double> operator*(Complex<double> c1, Complex<int> c2)
+{
+    Complex<double> temp((double)c2.re,(double)c2.im);
+    return temp.add2(c1);
+}
+
+Complex<double> operator*(Complex<Rational> c1, Complex<double> c2)
+{
+    Complex<double> temp(c1.re.getDouble(), c1.im.getDouble());
+    return temp.add2(c2);
+}
+
+Complex<double> operator*(Complex<double> c1, Complex<Rational> c2)
+{
+    Complex<double> temp(c2.re.getDouble(), c2.im.getDouble());
+    return temp.add2(c1);
+}
+
+Complex<double> operator*(Complex<BigInt> c1, Complex<double> c2)
+{
+    Complex<double> temp((double)c1.re.getLongLong(),(double)c1.im.getLongLong());
+    return temp.add2(c2);
+}
+
+Complex<double> operator*(Complex<double> c1, Complex<BigInt> c2)
+{
+    Complex<double> temp((double)c2.re.getLongLong(),(double)c2.im.getLongLong());
+    return temp.add2(c1);
+}
+
+Complex<double> operator*(Complex<double> c1, Complex<double> c2)
+{
+    return c1.add2(c2);
+}
+
+Complex<Rational> operator*(Complex<BigInt> c1, Complex<Rational> c2)
+{
+    Rational temp1(c1.re.getInt(),1);
+    Rational temp2(c1.im.getInt(),1);
+    Complex<Rational> temp(temp1,temp2);
+    return temp.add2(c2);
+}
+
+Complex<Rational> operator*(Complex<Rational> c1, Complex<BigInt> c2)
+{
+    Rational temp1(c2.re.getInt(),1);
+    Rational temp2(c2.im.getInt(),1);
+    Complex<Rational> temp(temp1,temp2);
+    return temp.add2(c1);
+}
+
+Complex<Rational> operator*(Complex<int> c1, Complex<Rational> c2)
+{
+    Rational temp1(c1.re);
+    Rational temp2(c1.im);
+    Complex<Rational> temp(temp1,temp2);
+    return temp.add2(c2);
+}
+
+Complex<Rational> operator*(Complex<Rational> c1, Complex<int> c2)
+{
+    Rational temp1(c2.re);
+    Rational temp2(c2.im);
+    Complex<Rational> temp(temp1,temp2);
+    return temp.add2(c1);
+}
+
+Complex<Rational> operator*(Complex<Rational> c1, Complex<Rational> c2)
+{
+    return c1.add2(c2);
+}
+
+Complex<BigInt> operator*(Complex<int> c1, Complex<BigInt> c2)
+{
+    BigInt temp1(c1.re);
+    BigInt temp2(c1.im);
+    Complex<BigInt> temp(temp1, temp2);
+    return temp.add2(c2);
+}
+
+Complex<BigInt> operator*(Complex<BigInt> c1, Complex<int> c2)
+{
+    BigInt temp1(c2.re);
+    BigInt temp2(c2.im);
+    Complex<BigInt> temp(temp1, temp2);
+    return temp.add2(c1);
+}
+
+Complex<BigInt> operator*(Complex<BigInt> c1, Complex<BigInt> c2)
+{
+    return c1.add2(c2);
+}
+
+Complex<int> operator*(Complex<int> c1, Complex<int> c2)
+{
+    return c1.add2(c2);
+}
+//---------------------------------------------------------------------------------------
 template <class type1> 
 Complex <type1> :: Complex()
 {
@@ -197,7 +444,7 @@ Complex <type1> Complex <type1> :: add(Complex &c)
 }
 
 template <class type1> 
-Complex <type1> Complex <type1> :: operator-(Complex &c)
+Complex <type1> Complex <type1> :: add1(Complex &c)
 {
 	Complex temp;
 	temp.re = re - c.re;
@@ -206,16 +453,10 @@ Complex <type1> Complex <type1> :: operator-(Complex &c)
 }
 
 template <class type1> 
-Complex <type1> Complex <type1> :: operator*(Complex &c)
+Complex <type1> Complex <type1> :: add2(Complex &c)
 {
 	Complex temp;
 	temp.re = re * c.re - im * c.im;
 	temp.im = re * c.im + im * c.re;
 	return temp;
-}
-
-template <class type1>
-void Complex <type1> :: show()
-{
-    cout << re << " + " << im << "i" <<endl;
 }
